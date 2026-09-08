140217967438699b42c5601305880d030bf921e2
studiocompro <studiocompro@ikmail.com>
2026-09-08 02:03:47 +0200

    Update ad slot IDs in ads-config.js

--- assets/ads-config.js
+++ assets/ads-config.js
@@ -3,9 +3,9 @@ window.CREATORRANK_ADS = {
 
   slots: {
     "home-leaderboard-01": "3050943352",
-    "home-content-01": "",
-    "breakout-inline-01": "",
-    "games-inline-01": "",
-    "justice-inline-01": ""
+    "home-content-01": "2643749246",
+    "breakout-inline-01": "9672192027",
+    "games-inline-01": "4172453333",
+    "justice-inline-01": "9943654764"
   }
 };
