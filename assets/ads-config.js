2cf988f85801aa4481d0d53df9d63e960a4a9ca4
studiocompro <studiocompro@ikmail.com>
2026-09-08 02:00:21 +0200

    Update ad slot ID for home leaderboard

--- assets/ads-config.js
+++ assets/ads-config.js
@@ -2,7 +2,7 @@ window.CREATORRANK_ADS = {
   client: "ca-pub-8281021937433044",
 
   slots: {
-    "home-leaderboard-01": "",
+    "home-leaderboard-01": "3050943352",
     "home-content-01": "",
     "breakout-inline-01": "",
     "games-inline-01": "",
